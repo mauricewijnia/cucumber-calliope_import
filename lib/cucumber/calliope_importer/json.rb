7019d2df70de878a2b14c751fba25ba647bb9e86
Maurice Wijnia <maurice.wijnia@spritecloud.com>
2017-10-18 14:08:53 +0200

    Added authorization check before the testsuite runs

--- lib/cucumber/calliope_importer/json.rb
+++ lib/cucumber/calliope_importer/json.rb
@@ -6,11 +6,21 @@ module Cucumber
       def initialize(config)
         super
         if ENV['API_KEY'].nil? || ENV['PROFILE_ID'].nil?
-          print "\033[31m" # Sets console colors to red
-          print  "Error: API Key and/or Profile ID were not set. Please provide them in your cucumber call or profile like this API_KEY='API Key Here' PROFILE_ID='Profile ID Here'"
-          print "\033[0m" # Resets console colors
+          error = "API Key and/or Profile ID were not set. Please provide them in your cucumber call or profile like this API_KEY='API Key Here' PROFILE_ID='Profile ID Here'"
+          print_error(error)
           exit
         end
+        #Set calliope data
+        @calliope_config = {
+            'api_key' => ENV['API_KEY'],
+            'profile_id' => ENV['PROFILE_ID']
+        }
+        if ENV['API_URL']
+          @calliope_config['url'] = ENV['API_URL']
+        else
+          @calliope_config['url'] = 'https://app.calliope/pro'
+        end
+        check_profile_permission
       end
 
       def on_finished_testing(event)
@@ -20,41 +30,61 @@ module Cucumber
       end
 
       def upload_to_api(results)
-        # Set data for api_call
+        # Get metadata from test run
         metadata = JSON.parse(File.read('./storage/metadata.json'))
-        calliope_config = {
-            'api_key' => ENV['API_KEY'],
-            'profile_id' => ENV['PROFILE_ID'],
-            'browser' => metadata['browser']['name'],
-            'os' => metadata['browser']['platform']
-        }
+        @calliope_config['browser'] = metadata['browser']['name']
+        @calliope_config['os'] = metadata['browser']['platform']
         # Set url
-        if ENV['API_URL']
-          calliope_config['url'] = ENV['API_URL']
-        else
-          calliope_config['url'] = 'https://app.calliope/pro'
-        end
-        api_call(results, calliope_config)
+        api_call(results)
       end
 
-      def api_call(results, calliope_config)
+      def api_call(results)
         # Make API call
-        url = "#{calliope_config['url']}/api/v2/profile/#{calliope_config['profile_id']}/testresult?browser=#{calliope_config['browser']}&os=#{calliope_config['os']}"
+        url = "#{@calliope_config['url']}/api/v2/profile/#{@calliope_config['profile_id']}/testresult?browser=#{@calliope_config['browser']}&os=#{@calliope_config['os']}"
         url = URI.parse(url)
         http = Net::HTTP.new(url.host, url.port)
         # Enable ssl when necessary
-        if calliope_config['url'].include? 'https://'
+        if @calliope_config['url'].include? 'https://'
           http.use_ssl = true
           http.verify_mode = OpenSSL::SSL::VERIFY_NONE
         end
         header = {
             'content-type' => 'application/json',
-            'x-api-key' => calliope_config['api_key']
+            'x-api-key' => @calliope_config['api_key']
         }
         request = Net::HTTP::Post.new(url, header)
         request.body = results
         response = http.request(request)
         print "\nAPI response: #{response.body} \n"
+      end
+
+      def check_profile_permission
+        url = "#{@calliope_config['url']}/api/v2/profile/#{@calliope_config['profile_id']}"
+        url = URI.parse(url)
+        http = Net::HTTP.new(url.host, url.port)
+        # Enable ssl when necessary
+        if @calliope_config['url'].include? 'https://'
+          http.use_ssl = true
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        end
+        header = {
+            'content-type' => 'application/json',
+            'x-api-key' => @calliope_config['api_key']
+        }
+        request = Net::HTTP::Get.new(url, header)
+        response = http.request(request)
+
+        if response.code != '200'
+          error = 'Not authorized for this calliope profile, exiting...'
+          print_error(error)
+          exit
+        end
+      end
+
+      def print_error(message)
+        print "\033[31m" # Sets console colors to red
+        print  "Error: #{message}"
+        print "\033[0m" # Resets console colors
       end
     end
   end
--- lib/cucumber/calliope_importer/version.rb
+++ lib/cucumber/calliope_importer/version.rb
@@ -1,5 +1,5 @@
 module Cucumber
   module CalliopeImporter
-    VERSION = "0.1.3"
+    VERSION = "0.1.4"
   end
 end
